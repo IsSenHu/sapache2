e5d1fb27d7abd2cd962aae2dbba8719bdbfb23f2
issenhu <1178515826@qq.com>
2019-05-30 20:05:18 +0800

    ok

--- ssaw-config/sdk/src/main/java/com/ssaw/config/sdk/config/ConfigAutoConfiguration.java
+++ ssaw-config/sdk/src/main/java/com/ssaw/config/sdk/config/ConfigAutoConfiguration.java
@@ -1,8 +1,12 @@
 package com.ssaw.config.sdk.config;
 
 import com.ssaw.config.sdk.annotation.EnableConfig;
+import com.ssaw.config.sdk.fallback.ConfigFeignImpl;
+import feign.Logger;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -10,8 +14,26 @@ import org.springframework.context.annotation.Configuration;
  * @date 2019/5/30 17:04
  */
 @Configuration
-@EnableFeignClients(basePackages = "com.ssaw.config.sdk")
+@EnableFeignClients(basePackages = "com.ssaw.config.sdk.feign")
 @ConditionalOnBean(annotation = EnableConfig.class)
 public class ConfigAutoConfiguration {
 
+    /**
+     * NONE:不记录任何信息
+     * BASIC:仅记录请求方法，URL以及响应状态码和执行时间
+     * HEADERS:除了记录BASIC级别的信息外，还会记录请求和响应的头信息
+     * FULL:记录所有请求与响应的明细，包括头信息，请求体，元数据
+     *
+     * @return 日志级别
+     */
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
+
+    @ConditionalOnMissingBean(ConfigFeignImpl.class)
+    @Bean
+    public ConfigFeignImpl configFeignImpl() {
+        return new ConfigFeignImpl();
+    }
 }
