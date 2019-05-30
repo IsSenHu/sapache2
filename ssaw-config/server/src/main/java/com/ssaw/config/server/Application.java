ce9bb1d8826c5e4f0bca750206f2e6c04d7459ae
issenhu <1178515826@qq.com>
2019-05-30 20:14:24 +0800

    ok

--- ssaw-config/server/src/main/java/com/ssaw/config/server/Application.java
+++ ssaw-config/server/src/main/java/com/ssaw/config/server/Application.java
@@ -1,5 +1,6 @@
 package com.ssaw.config.server;
 
+import com.ssaw.commons.enable.EnableAutoRequestResolve;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -8,6 +9,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  * @author HuSen
  * @date 2019/5/30 16:39
  */
+@EnableAutoRequestResolve
 @EnableConfigServer
 @SpringBootApplication
 public class Application {
