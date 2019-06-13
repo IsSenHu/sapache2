d602b688495fd640ff229a0273da4c362b30f3c4
user <>
2019-06-13 12:50:42 +0800

    return

--- ssaw-config/sdk/src/main/java/com/ssaw/config/sdk/vo/ConfigViewVO.java
+++ ssaw-config/sdk/src/main/java/com/ssaw/config/sdk/vo/ConfigViewVO.java
@@ -40,7 +40,6 @@ public class ConfigViewVO {
     /**
      * 修改时间
      * */
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private LocalDateTime modifyTime;
 
     /**
@@ -51,7 +50,6 @@ public class ConfigViewVO {
     /**
      * 创建时间
      * */
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private LocalDateTime createTime;
 
     /**
